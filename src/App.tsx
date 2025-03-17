97fb95ad274f2cbcfb83bfc869e588f028d1f21e
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2025-03-17 16:18:43 +0100

    adj mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Layout from "./components/ui/Layout.tsx";
-import { ThemeProvider } from "./components/theme.tsx";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 import TodoList from "./components/TodoList.tsx";
 import AddTodo from "./components/AddTodo.tsx";
 import Filter from "./components/Filter.tsx";
--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,11 +1,12 @@
 import SataticInfo from "../SataticInfo";
+import Modetoggle from "../Modetoggle";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <header className="m-10 flex justify-between">
         <h1 className="text-2xl font-bold">Todo App</h1>
-        <button className="text-2xl font-bold"> ⁕</button>
+        <Modetoggle />
       </header>
 
       <main className="m-10">{children}</main>
